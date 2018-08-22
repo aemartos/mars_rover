ca72e7583fe52c0562c207768a62a80d5f29cad7
aemartos <aemartos@gmail.com>
2018-08-22 14:58:22 +0200

    Direction movement

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
 // Rover Object Goes Here
 // ======================
 
+var roverling = {
+  direction: "N"
+}
+
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  switch (roverling.direction) {
+    case "N":
+      roverling.direction = "W"
+      break;
+    case "E":
+      roverling.direction = "N"
+      break;
+    case "S":
+      roverling.direction = "E"
+      break;
+    case "W":
+      roverling.direction = "S"
+      break;
+  }
+  console.log(roverling.direction);
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch (roverling.direction) {
+    case "N":
+      roverling.direction = "E"
+      break;
+    case "E":
+      roverling.direction = "S"
+      break;
+    case "S":
+      roverling.direction = "W"
+      break;
+    case "W":
+      roverling.direction = "N"
+      break;
+  }
+  console.log(roverling.direction);
 }
 
 function moveForward(rover){
